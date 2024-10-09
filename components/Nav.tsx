b77f69572b9a581f3be548ce320f442d002b5cdd
Gaurav Singh <gauravrajputsingh94@gmail.com>
2024-10-10 01:44:04 +0530

    done some changes in navbar.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-white shadow-md w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
